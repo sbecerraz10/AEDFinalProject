a42e737028760d448f695a1c2551252fe8680b50
CAMILO <Kmilo0919@gmail.com>
2019-05-27 13:20:54 -0500

    Interface

---
+++ AEDFinalProject/src/application/PathsController.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class PathsController implements Initializable{
+
+    @FXML
+    private Button btFloyd;
+
+    @FXML
+    private Button btDfs;
+
+    @FXML
+    private TextArea info;
+
+    @FXML
+    void dfs(ActionEvent event) {
+
+    }
+
+    @FXML
+    void floyd(ActionEvent event) {
+
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
